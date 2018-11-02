63f9045edf0f0dd40425e8b5660eb49ac573c8b4
Mwilson7992 <Mwilson091416@gmail.com>
2018-11-02 12:08:55 -0500

    imgs now gifs

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,67 @@
+var startScreen;
+    var gameHTML;
+    var counter = 30;
+    var questionArray = 
+    ["Dead creatures revived by white walkers are known as what?",
+    "Who created the secret tunnel in the sewers of Casterly Rock?",
+    "The Night King was created using a dagger made of what?", 
+    "Prince Oberyn Martell is nicknamed the 'Red Viper' because of his combat skills and what else?", 
+    "How did Daenerys hatch her dragon eggs?", 
+    "What does the phrase 'Valor Morghulis' stand for?", 
+    "Actor Peter Dinklage, Tyrion Lannister, also had a starring role in what fantasy franchise?", 
+    "What is the only thing that can put out volatile wildfire?",
+    "Besides dragonglass, what is the only other substance capable of defeating a white walker?",
+    "How many times has Beric Dondarrion been brought back to life?",
+    "Which Stark family direwolf was slain in retaliation for an attack on Prince Joffrey?",
+    "Aryas's punishment for stealing from the Many-Face God is what?",
+    "The name of King Tommens favorite cat is?",
+    "What was the name of Ned Starks greatsword?",
+    "Who shoots the flaming arror that subsequently destroys Stannis' fleet in Blackwater Bay?"];
+
+    var answerArray = 
+    [["Walkers", "Wights", "Zombies", "Creepy Claws"], 
+    ["Tyrion Lannister","Lord Baelish","Jaime Lannister","Varys"], 
+    ["Valyrian Steel", "Dragonglass", "Blue Ice", "Obsidian"], 
+    ["Pride in drawing first blood","Knowledge of poisons","Nighttime attacks","Ruby-colored armor"], 
+    ["Lightning Storm", "Funeral Pyre", "Fireplace", "Frozen Cave"], 
+    ["All Men Must Die","All Men Must Serve","All Men Must Live","All Men Must See"], 
+    ["Lord of the Rings", "Highlander", "Chronicles of Narnia", "Legend of Zelda"], 
+    ["Sand","Water","Sunlight","Dragons Blood"],
+    ["Weirwood","Wildfire","Valyrian Steel","Snowballs"],
+    ["Three","Five","Seven","Six"],
+    ["Ghost","Lady","Summer","Nymeria"],
+    ["Death","Memory Loss","Blindness","Deafness"],
+    ["Ser Pounce","Prince Fuzzy","Little Lion","Hairball"],
+    ["Oathkeeper","Widows Wail","Longclaw","Ice"],
+    ["Tyrion","Joffrey","Jaime","Bronn"]
+    ];
+
+    var imageArray = 
+    ["<img class='center-block img-right' src='assets/images/wight.gif'>", 
+    "<img class='center-block img-right' src='assets/images/tyrion.gif'>", 
+    "<img class='center-block img-right' src='assets/images/dragonglass.gif'>", 
+    "<img class='center-block img-right' src='assets/images/oberyn.gif'>", 
+    "<img class='center-block img-right' src='assets/images/daenerys.gif'>", 
+    "<img class='center-block img-right' src='assets/images/valer.gif'>", 
+    "<img class='center-block img-right' src='assets/images/narnia.jpg'>", 
+    "<img class='center-block img-right' src='assets/images/wildfire.gif'>",
+    "<img class='center-block img-right' src='assets/images/valyrian.gif'>",
+    "<img class='center-block img-right' src='assets/images/beric.gif'>",
+    "<img class='center-block img-right' src='assets/images/lady.gif'>",
+    "<img class='center-block img-right' src='assets/images/arya.gif'>",
+    "<img class='center-block img-right' src='assets/images/pounce.gif'>",
+    "<img class='center-block img-right' src='assets/images/ned.gif'>",
+    "<img class='center-block img-right' src='assets/images/bronn.gif'>"];
+
+    var correctAnswers = ["B. Wights", "A. Tyrion Lannister", "B. Dragonglass", "B. Knowledge of poisons", "B. Funeral Pyre", "A. All Men Must Die", "C. Chronicles of Narnia", "A. Sand", "C. Valyrian Steel", "D. Six", "B. Lady", "C. Blindness", "A. Ser Pounce", "D. Ice", "D. Bronn"];
+    var questionCounter = 0;
+    var selecterAnswer;
+    var Clock;
+    var correctTally = 0;
+    var incorrectTally = 0;
+    var unansweredTally = 0;
+    var clickSound = new Audio("sound/button-click.mp3");
+    
 $(document).ready(function() {
     
     function initialScreen() {
@@ -92,7 +156,7 @@ $(document).ready(function() {
     }
     
     function finalScreen() {
-        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>";
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + counter + "</span></p>" + "<p class='text-center'>All done, here's how you did!" + "</p>" + "<p class='summary-correct'>Correct Answers: " + correctTally + "</p>" + "<p>Wrong Answers: " + incorrectTally + "</p>" + "<p>Unanswered: " + unansweredTally + "</p>" + "<p class='text-center reset-button-container'><a class='btn btn-primary btn-lg btn-block reset-button' href='#' role='button'>Reset The Quiz!</a></p>" + "<img class='center-block img-wrong' src='assets/images/wrong.jpg'>";
         $(".mainArea").html(gameHTML);
     }
     
@@ -106,66 +170,4 @@ $(document).ready(function() {
         timerWrapper();
     }
     
-    var startScreen;
-    var gameHTML;
-    var counter = 30;
-    var questionArray = 
-    ["Dead creatures revived by white walkers are known as what?",
-    "Who created the secret tunnel in the sewers of Casterly Rock?",
-    "The Night King was created using a dagger made of what?", 
-    "Prince Oberyn Martell is nicknamed the 'Red Viper' because of his combat skills and what else?", 
-    "How did Daenerys hatch her dragon eggs?", 
-    "What does the phrase 'Valor Morghulis' stand for?", 
-    "Actor Peter Dinklage, Tyrion Lannister, also had a starring role in what fantasy franchise?", 
-    "What is the only thing that can put out volatile wildfire?",
-    "Besides dragonglass, what is the only other substance capable of defeating a white walker?",
-    "How many times has Beric Dondarrion been brought back to life?",
-    "Which Stark family direwolf was slain in retaliation for an attack on Prince Joffrey?",
-    "Aryas's punishment for stealing from the Many-Face God is what?",
-    "The name of King Tommens favorite cat is?",
-    "What was the name of Ned Starks greatsword?",
-    "Who shoots the flaming arror that subsequently destroys Stannis' fleet in Blackwater Bay?"];
-
-    var answerArray = 
-    [["Walkers", "Wights", "Zombies", "Creepy Claws"], 
-    ["Tyrion Lannister","Lord Baelish","Jaime Lannister","Varys"], 
-    ["Valyrian Steel", "Dragonglass", "Blue Ice", "Obsidian"], 
-    ["Pride in drawing first blood","Knowledge of poisons","Nighttime attacks","Ruby-colored armor"], 
-    ["Lightning Storm", "Funeral Pyre", "Fireplace", "Frozen Cave"], 
-    ["All Men Must Die","All Men Must Serve","All Men Must Live","All Men Must See"], 
-    ["Lord of the Rings", "Highlander", "Chronicles of Narnia", "Legend of Zelda"], 
-    ["Sand","Water","Sunlight","Dragons Blood"],
-    ["Weirwood","Wildfire","Valyrian Steel","Snowballs"],
-    ["Three","Five","Seven","Six"],
-    ["Ghost","Lady","Summer","Nymeria"],
-    ["Death","Memory Loss","Blindness","Deafness"],
-    ["Ser Pounce","Prince Fuzzy","Little Lion","Hairball"],
-    ["Oathkeeper","Widows Wail","Longclaw","Ice"],
-    ["Tyrion","Joffrey","Jaime","Bronn"]
-    ];
-
-    var imageArray = 
-    ["<img class='center-block img-right' src='assets/images/wight.png'>", 
-    "<img class='center-block img-right' src='assets/images/tyrion.jpg'>", 
-    "<img class='center-block img-right' src='assets/images/dragonglass.jpg'>", 
-    "<img class='center-block img-right' src='assets/images/oberyn.jpg'>", 
-    "<img class='center-block img-right' src='assets/images/daenerys.jpg'>", 
-    "<img class='center-block img-right' src='assets/images/valer.jpg'>", 
-    "<img class='center-block img-right' src='assets/images/narnia.jpg'>", 
-    "<img class='center-block img-right' src='assets/images/sand.jpg'>",
-    "<img class='center-block img-right' src='assets/images/valyrian.jpg'>",
-    "<img class='center-block img-right' src='assets/images/beric.jpeg'>",
-    "<img class='center-block img-right' src='assets/images/lady.jpg'>",
-    "<img class='center-block img-right' src='assets/images/arya.jpg'>",
-    "<img class='center-block img-right' src='assets/images/pounce.jpg'>",
-    "<img class='center-block img-right' src='assets/images/ned.jpg'>",
-    "<img class='center-block img-right' src='assets/images/bronn.jpg'>"];
-
-    var correctAnswers = ["B. Wights", "A. Tyrion Lannister", "B. Dragonglass", "B. Knowledge of poisons", "B. Funeral Pyre", "A. All Men Must Die", "C. Chronicles of Narnia", "A. Sand", "C. Valyrian Steel", "D. Six", "B. Lady", "C. Blindness", "A. Ser Pounce", "D. Ice", "D. Bronn"];
-    var questionCounter = 0;
-    var selecterAnswer;
-    var Clock;
-    var correctTally = 0;
-    var incorrectTally = 0;
-    var unansweredTally = 0;
-    var clickSound = new Audio("sound/button-click.mp3");
+    
